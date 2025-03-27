5f2e60c7c3092930e53338b6e5b33b2077429b87
bilal <mxbilal0@gmail.com>
2025-03-27 10:42:39 +0500

    fx

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/automtaion-test/", // Must match your repo name exactly
+  base: "/", // Must match your repo name exactly
 });
